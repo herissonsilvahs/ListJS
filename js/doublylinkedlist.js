c0b380b3c2810776de30ae2045571ca8905137bd
Herisson Silva <herissonsilvahs@gmail.com>
2018-09-19 17:07:56 -0300

    First

--- js/doublylinkedlist.js
+++ js/doublylinkedlist.js
@@ -16,7 +16,44 @@ class DoublyLinkedList extends LinkedList{
         this.lenght++
     }
 
-    insert(position, element){}
+    insert(position, element){
+        if(position>=0 && position<=this.lenght){
+            let node = new Node(element)
+            let current = this.head
+            let index = 0
 
-    removeAt(position) {}
+            if(position==0){
+
+                if (!this.head){
+                    this.head = node
+                    this.tail = node
+                }else{
+                    node.next=this.head
+                    this.head.previous = node
+                    this.head = node
+                }
+
+            }else if(position==this.lenght){
+                node.previous = this.tail
+                this.tail.next = node
+                this.tail = node
+            }else{
+                while(index<position){
+                    index++
+                    current=current.next
+                }
+                current.previous.next = node
+                node.previous = current.previous
+                current.previous = node
+            }
+
+            this.lenght++
+            return true
+        }
+        return false
+    }
+
+    removeAt(position) {
+        
+    }
 }
